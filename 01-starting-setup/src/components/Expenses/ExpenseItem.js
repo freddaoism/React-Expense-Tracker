31fc7dc6d29625bb585f18327464302bfbe64660
Fred Dao <freddaoism@gmail.com>
2021-06-23 14:53:51 -0400

    Working on state
    In the middle of app state changes

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+import React, { useState } from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+    setTitle('U up-dated?');
+    console.log(title);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
