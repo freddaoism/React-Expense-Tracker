c84ef6aa00c414653a32507c4847b12299bc3723
Fred Dao <freddaoism@gmail.com>
2021-07-30 15:30:27 -0400

    Rendering lists, working with state, and conditional content
    Added lists, state, and conditional content.

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,37 +1,42 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, {useState} from 'react';
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Bear Skin Rug",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { 
-    id: "e2", 
-    title: "Life Insurance", 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12) 
-    },
-    {
-      id: "e3",
-      title: "Cucumber",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450.33,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Bear Skin Rug",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+  id: "e2", 
+  title: "Life Insurance", 
+  amount: 799.49, 
+  date: new Date(2021, 2, 12) 
+  },
+  {
+    id: "e3",
+    title: "Cucumber",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450.33,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+        return [expense, ...prevExpenses];    
+    });
   };
 
   return (
--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,17 @@
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
-import React, { useState } from 'react';
+import React from 'react';
 
 const ExpenseItem = (props) => {
-  
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-
-  const clickHandler = () => {
-    setTitle('U up-dated?');
-    console.log(title);
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      
     </Card>
   );
 }
