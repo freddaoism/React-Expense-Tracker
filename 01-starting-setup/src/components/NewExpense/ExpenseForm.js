07b23cb101a49792d694a0db58da30f21714001c
Fred Dao <freddaoism@gmail.com>
2021-08-01 18:48:19 -0400

    Fixing conditional output bugs
    Fixing conditional output bugs

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -98,6 +98,7 @@ const ExpenseForm = (props) => {
           <input type="date" value={enteredAmount} min="2019-01-01" max="2022-12-" onChange={dateChangeHandler} />
         </div>
         <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>Cancel</button>
           <button type="submit">Add Expense</button>
         </div>
       </div>
